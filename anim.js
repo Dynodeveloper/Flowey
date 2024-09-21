d301efe221564abf9cc06f09d476a522deb660dd
Cesar <cesardyno0207@gmail.com>
2024-09-21 17:49:21 -0500

    ajuste de tiempos

--- anim.js
+++ anim.js
@@ -4,31 +4,34 @@ var lyrics = document.querySelector("#lyrics");
 
 // Array de objetos que contiene cada línea y su tiempo de aparición en segundos
 var lyricsData = [
-  { text: "un sweater violeta y", time: 0 },
-  { text: "un pantalón de corderoy", time: 3 },
-  { text: "la pasó a buscar en su nuevo renault fuego", time: 7 },
-  { text: "sonaban los planetas y la furia de la autopista", time: 11 },
-  { text: "de pronto ya estaba en el auto", time: 15 },
-  { text: "y sin apuro preguntó", time: 19 },
-  { text: "ey, dónde me llevás", time: 22 },
-  { text: "y cuánto más vas a tardar", time: 25 },
-  { text: "es que necesito volver a casa para cenar", time: 29 },
-  { text: "la pasaron bien un rato", time: 34 },
-  { text: "dos horas para ser exactos", time: 38 },
-  { text: "él le dio un poco de pánico", time: 42 },
-  { text: "y ella se quedó pensando", time: 46 },
-  { text: "ey, dónde me llevás", time: 50 },
-  { text: "y cuánto más vas a tardar", time: 53 },
-  { text: "es que necesito volver a casa para cenar", time: 57 },
-  { text: "puede que no haya un auto y", time: 62 },
-  { text: "que casi todo sea inventado", time: 65 },
-  { text: "es mejor inventar un buen final", time: 69 },
-  { text: "para no poder pensar en...", time: 73 },
-  { text: "ey, dónde te fuiste", time: 78 },
-  { text: "y cuánto más vas a tardar", time: 82 },
-  { text: "es que necesito decirte que en verdad me gustas", time: 87 },
-  { text: "en verdad me gustabas", time: 92 },
+  { text: "un sweater violeta y", time: 20 },
+  { text: "un pantalón de corderoy", time: 23 },
+  { text: "la pasó a buscar en su nuevo renault fuego", time: 27 },
+  { text: "sonaban los planetas y la furia de la autopista", time: 31 },
+  { text: "de pronto ya estaba en el auto", time: 36 },
+  { text: "y sin apuro preguntó", time: 39 },
+  { text: "ey, dónde me llevás", time: 42 },
+  { text: "y cuánto más vas a tardar", time: 45 },
+  { text: "es que necesito volver a casa para cenar", time: 47 },
+  { text: "la pasaron bien un rato", time: 66 },
+  { text: "dos horas para ser exactos", time: 68 },
+  { text: "él le dio un poco de pánico", time: 70 },
+  { text: "y ella se quedó pensando", time: 72 },
+  { text: "ey, dónde me llevás", time: 74 },
+  { text: "y cuánto más vas a tardar", time: 76 },
+  { text: "es que necesito volver a casa para cenar", time: 78 },
+  { text: "puede que no haya un auto y", time: 97 },
+  { text: "que casi todo sea inventado", time: 99 },
+  { text: "es mejor inventar un buen final", time: 102 },
+  { text: "para no poder pensar en...", time: 104 },
+  { text: "ey, dónde te fuiste", time: 106 },
+  { text: "y cuánto más vas a tardar", time: 108 },
+  { text: "es que necesito decirte que en verdad me gustas", time: 110 },
+  { text: "en verdad me gustabas", time: 112 },
 ];
+
+
+
 
 
 
